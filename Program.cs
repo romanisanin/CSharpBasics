894e676e9051010ebbc952fc3d384155f1c2bcf7
romanisanin <romanisanin@gmail.com>
2021-10-26 21:58:43 -0400

    Task 1 Solution

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSharpBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter username:");
+            string username = Console.ReadLine();
+            Console.WriteLine($"Hello, {username}, today is {DateTime.Now.ToShortDateString()}");
+        }
+    }
+}
