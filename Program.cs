39a8d9966764bc6776626179af72389cc8add2c6
romanisanin <romanisanin@gmail.com>
2021-10-31 15:05:06 -0400

    Lesson 2/Task 1 Solution

--- Program.cs
+++ Program.cs
@@ -6,9 +6,12 @@ namespace CSharpBasics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter username:");
-            string username = Console.ReadLine();
-            Console.WriteLine($"Hello, {username}, today is {DateTime.Now.ToShortDateString()}");
+            Console.WriteLine("Enter min temperature in °C:");
+            float min =  float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter max temperature in °C:");
+            float max = float.Parse(Console.ReadLine());
+            Console.WriteLine($"Average day's temperature is {(max+min)/2}°C");
         }
     }
 }
+//Запросить у пользователя минимальную и максимальную температуру за сутки и вывести среднесуточную температуру.
