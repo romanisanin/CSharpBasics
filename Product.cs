b11c978f4c407b2f944da508daa7aa54c2839c76
romanisanin <romanisanin@gmail.com>
2021-10-31 22:01:49 -0400

    Lesson2/Task4

---
+++ Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBasics
+{
+    class Product
+    {
+        string productName;
+        float productPrice;
+        float productAmount;
+
+        public Product(string _productName, float _productPrice, float _productAmount)
+        {
+            productName = _productName;
+            productPrice = _productPrice;
+            productAmount = _productAmount;
+        }
+
+        public Tuple<string,float> printProduct()
+        {
+            float price = productPrice * productAmount;
+            if (productName.Length > 20)
+            {
+                productName = productName.Substring(0, 19);
+            }
+            string productDetail = $"{productName}\n{productPrice}X{productAmount}\nPrice:............{price}";
+
+            return Tuple.Create(productDetail, price);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CSharpBasics
 {
@@ -6,9 +7,61 @@ namespace CSharpBasics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter username:");
-            string username = Console.ReadLine();
-            Console.WriteLine($"Hello, {username}, today is {DateTime.Now.ToShortDateString()}");
+            Console.WriteLine("Enter receipt #");
+            string receipt = Console.ReadLine();
+            List<Product> productList = new List<Product>();
+            bool stop = false;
+            while (!stop)
+            {
+                float productPrice = 0;
+                float productAmount = 0;
+                Console.WriteLine("Enter product name. To Stop type Enter.");
+                string productName = Console.ReadLine();
+                if(productName == "") { stop = true; break; }
+                try
+                {
+                    Console.WriteLine("Enter product price:");
+                    productPrice = float.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    stop = true; break;
+                }
+                try
+                {
+                    Console.WriteLine("Enter product amount:");
+                    productAmount = float.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    stop = true; break;
+                }
+                productList.Add(new Product(productName, productPrice, productAmount));
+            }
+
+            if (productList.Count == 0)
+            {
+                Console.WriteLine("No products have been entered or something has entered incorrect. Check has been cancelled.");
+            }
+            else
+            {
+                int counter = 0;
+                float total = 0;
+
+                Console.WriteLine($"********************Receipt #{receipt}********************");
+                foreach (var item in productList)
+                {
+                    counter++;
+                    total += item.printProduct().Item2;
+                    Console.WriteLine($"{counter}. {item.printProduct().Item1}");
+                }
+
+                Console.WriteLine("_________________________");
+                Console.WriteLine($"Total: {total}");
+                Console.WriteLine($"{DateTime.Now.ToLongTimeString()}");
+            }
         }
     }
 }
+//Для полного закрепления понимания простых типов найдите любой чек, либо фотографию этого чека в интернете и схематично нарисуйте его в консоли, только за место динамических,
+//по вашему мнению, данных (это может быть дата, название магазина, сумма покупок) подставляйте переменные, которые были заранее заготовлены до вывода на консоль
