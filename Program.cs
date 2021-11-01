ef5dc4d33baab33e9461c8c3415bdd06cd662b02
romanisanin <romanisanin@gmail.com>
2021-10-31 22:23:23 -0400

    Lesson2/Task5

--- Program.cs
+++ Program.cs
@@ -4,11 +4,46 @@ namespace CSharpBasics
 {
     class Program
     {
+        enum WinterMonth
+        {
+            December,
+            January,
+            February
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter username:");
-            string username = Console.ReadLine();
-            Console.WriteLine($"Hello, {username}, today is {DateTime.Now.ToShortDateString()}");
+            float averageTemp;
+            Console.WriteLine("Enter a month:");
+            string month = Console.ReadLine();
+
+            if(month.ToLower()  == WinterMonth.December.ToString().ToLower() || 
+                month.ToLower() == WinterMonth.January.ToString().ToLower() || 
+                month.ToLower() == WinterMonth.February.ToString().ToLower())
+            {
+                Console.WriteLine("Enter average temperature:");
+                try
+                {
+                    averageTemp = float.Parse(Console.ReadLine());
+                    if (averageTemp > 0)
+                    {
+                        Console.WriteLine("Rainy Winter");
+                    }
+                    else
+                    { 
+                        Console.WriteLine("Snowy Winter");
+                    }
+
+                }
+                catch
+                {
+                    Console.WriteLine("Wrong temperature has been entered. Closing the app");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Wrong month has been entered. Closing the app");
+            }
+            //(*) Если пользователь указал месяц из зимнего периода, а средняя температура > 0, вывести сообщение «Дождливая зима».
         }
     }
 }
