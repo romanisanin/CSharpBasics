63bcd9f68bd614eefb3164965dd01496fba70527
romanisanin <romanisanin@gmail.com>
2021-11-03 22:15:43 -0400

    Lesson3/Task2

--- Program.cs
+++ Program.cs
@@ -6,9 +6,32 @@ namespace CSharpBasics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter username:");
-            string username = Console.ReadLine();
-            Console.WriteLine($"Hello, {username}, today is {DateTime.Now.ToShortDateString()}");
+            string[,] phonebook = new string[5, 2];
+
+            for (int i = 0; i < phonebook.GetLength(0); i++)
+            {
+                for (int j = 0; j < phonebook.GetLength(1); j++)
+                {
+                    if (j == 0)
+                    {
+                        Console.WriteLine($"Enter the contact name #{i + 1}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Enter the phone number for contact #{i + 1}");
+                    }
+                    phonebook[i, j] = Console.ReadLine();                }
+            }
+
+            for (int i = 0; i < phonebook.GetLength(0); i++)
+            {
+                for (int j = 0; j < phonebook.GetLength(1); j++)
+                {
+                    Console.Write($"{phonebook[i, j]} ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
+//Написать программу — телефонный справочник — создать двумерный массив 5*2, хранящий список телефонных контактов: первый элемент хранит имя контакта, второй — номер телефона/e-mail.
