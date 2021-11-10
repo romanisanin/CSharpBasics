ab3eb7ff3ac555d7e18bb8e4d8e7210979e47a25
romanisanin <romanisanin@gmail.com>
2021-11-09 22:44:59 -0500

    Lesson4/Task1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CSharpBasics
 {
@@ -6,9 +7,41 @@ namespace CSharpBasics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter username:");
-            string username = Console.ReadLine();
-            Console.WriteLine($"Hello, {username}, today is {DateTime.Now.ToShortDateString()}");
+            List<string> catalog = new List<string>();
+            AddUsers(catalog);
+            
+            int counter = 0;
+            foreach (var FIO in catalog)
+            {
+                Console.WriteLine($"{++counter}. {FIO}");
+            }
+        }
+
+        static void AddUsers(List<string> catalog)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                catalog.Add(AddUser(i+1));
+            }
+        }
+
+        static string AddUser(int iterator)
+        {
+            Console.WriteLine($"Enter FirstName #{iterator}:");
+            string firstName = Console.ReadLine();
+            Console.WriteLine($"Enter LastName #{iterator}:");
+            string lastName = Console.ReadLine();
+            Console.WriteLine($"Enter Patronymic #{iterator}:"); 
+            string patronymic = Console.ReadLine();
+            return GetFullName(firstName, lastName, patronymic);
+        }
+
+        private static string GetFullName(string firstName, string lastName, string patronymic)
+        {
+            return lastName +" "+ firstName +" "+ patronymic;
         }
     }
 }
+//Написать метод GetFullName(string firstName, string lastName, string patronymic),
+//принимающий на вход ФИО в разных аргументах и возвращающий объединённую строку с ФИО.
+//Используя метод, написать программу, выводящую в консоль 3–4 разных ФИО.
