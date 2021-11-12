ed0e930db0905cb9e6f607821d175a5aae7c7906
romanisanin <romanisanin@gmail.com>
2021-11-11 19:51:20 -0500

    Lesson4/Task2

--- Program.cs
+++ Program.cs
@@ -6,9 +6,24 @@ namespace CSharpBasics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter username:");
-            string username = Console.ReadLine();
-            Console.WriteLine($"Hello, {username}, today is {DateTime.Now.ToShortDateString()}");
+            Console.WriteLine("Enter numbers set split by space:");
+            string set = Console.ReadLine();
+            int sum = 0;
+            for (int i = 0; i < set.Length; i++)
+            {
+                string s = "";
+                while(set[i].ToString() != " ")
+                {
+                    s += set[i].ToString();
+                    if (i == set.Length-1)
+                        break;
+                    i++;
+                }
+                sum += Int32.Parse(s); 
+            }
+            Console.WriteLine($"The sum is {sum}");
         }
     }
 }
+//Написать программу, принимающую на вход строку — набор чисел, разделенных пробелом,
+//и возвращающую число — сумму всех чисел в строке. Ввести данные с клавиатуры и вывести результат на экран.
