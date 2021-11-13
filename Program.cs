c3cd71e3995a5ea5f56e181b9512b8c8ae130697
romanisanin <romanisanin@gmail.com>
2021-11-12 22:57:15 -0500

    Lesson5/Task4

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace CSharpBasics
 {
@@ -6,9 +7,48 @@ namespace CSharpBasics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter username:");
-            string username = Console.ReadLine();
-            Console.WriteLine($"Hello, {username}, today is {DateTime.Now.ToShortDateString()}");
+            string workDir = @"D:\tmp";
+            string filename = "catalog.txt";
+
+            Console.WriteLine("--------------------Recursion------------------------");
+
+            Recursion(workDir);
+
+            Console.WriteLine("--------------------Recursion------------------------");
+            NoRecursion(workDir, filename);
+        }
+
+        private static void Recursion(string workDir)
+        {
+            try
+            {
+                foreach (string f in Directory.GetFiles(workDir))
+                    Console.WriteLine(f);
+                foreach (string d in Directory.GetDirectories(workDir))
+                {
+                    Console.WriteLine(d);
+                    Recursion(d);
+                }
+
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void NoRecursion(string workDir, string filename)
+        {
+            string[] entries = Directory.GetFileSystemEntries(workDir, "*", SearchOption.AllDirectories);
+
+            File.WriteAllLines(filename, entries);
+
+            string[] output = File.ReadAllLines(filename);
+            foreach (string entry in output)
+            {
+                Console.WriteLine(entry);
+            }
         }
     }
 }
+//(*) Сохранить дерево каталогов и файлов по заданному пути в текстовый файл — с рекурсией и без.
