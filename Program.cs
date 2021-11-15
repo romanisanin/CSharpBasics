48998b9bc078ed130bb0a073c08cff28301a3373
romanisanin <romanisanin@gmail.com>
2021-11-14 21:24:43 -0500

    Lesson6/Task1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace CSharpBasics
 {
@@ -6,9 +7,106 @@ namespace CSharpBasics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter username:");
-            string username = Console.ReadLine();
-            Console.WriteLine($"Hello, {username}, today is {DateTime.Now.ToShortDateString()}");
+            Process[] processes = GetProcessesList();
+
+            Console.WriteLine("Enter process id or process name to kill the process");
+            string proc = Console.ReadLine();
+            if (Int32.TryParse(proc, out int value))
+            {
+                KillProccessById(value,processes);
+            }
+            else
+            {
+                KillProcessByName(proc,processes);
+            }
+        }
+
+
+        static Process[] GetProcessesList()
+        {
+            Process[] processes = Process.GetProcesses();
+            foreach (var process in processes)
+            {
+                Console.WriteLine($"{process.Id} {process.ProcessName}");
+            }
+            return processes;
+        }
+
+        static void KillProcessByName(string processName, Process[] processes)
+        {
+            try
+            {
+                foreach (var process in processes)
+                {
+                    if (process.ProcessName == processName)
+                        process.Kill();
+                }
+                CheckIfKilled(processes, processName);
+                
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);    
+            }
+        }
+
+        static void KillProccessById(int processId, Process[] processes)
+        {
+            try 
+            { 
+                foreach (var process in processes)
+                {
+                    if (process.Id == processId)
+                    {
+                        process.Kill();
+                        processes = GetProcessesList();
+                        CheckIfKilled(processes,processId);
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
+        }
+
+
+        static void CheckIfKilled(Process[] processes, int processId)
+        {
+            int counter = 0;
+            foreach (var process in processes)
+            {
+                if (process.Id == processId)
+                {
+                    counter++;
+                    throw new Exception("Something went wrong");
+                }
+            }
+            if (counter == 0)
+            {
+                Console.WriteLine($"The process id {processId} has been removed");
+            }
+        }
+
+        static void CheckIfKilled(Process[] processes, string processName)
+        {
+            int counter = 0;
+            foreach (var process in processes)
+            {
+                if (process.ProcessName == processName)
+                {
+                    counter++;
+                    throw new Exception("Something went wrong");
+                }
+            }
+            if (counter == 0)
+            {
+                Console.WriteLine($"The process name {processName} has been removed");
+            }
         }
     }
 }
+//Написать консольное приложение Task Manager, которое выводит на экран запущенные процессы и позволяет завершить указанный процесс.
+//Предусмотреть возможность завершения процессов с помощью указания его ID или имени процесса.
+//В качестве примера можно использовать консольные утилиты Windows tasklist и taskkill.
